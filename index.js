1a7e933af3749e1edc0fbee50589fd7995a612d8
michael.roger <michael.roger@wonga.com>
2017-12-08 16:13:01 +0000

    Fix callback.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict';
-var fs = require('fs');
+const fs = require('fs');
 
- exports.get = function(event, context, callback) {
-  callback(null, 'HELLO WORLDY!!');
- };
+exports.get = function(event, context, callback) {
+  const contents = fs.readFileSync("public/index.html");
+  const response = {
+    statusCode: 200,
+    body: contents.toString(),
+    headers: {'Content-Type': 'text/html'}
+  };
+  callback(null, response);
+};
