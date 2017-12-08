67a8988a865eb581b4fbb09426d18d5abb29b1d1
michael.roger <michael.roger@wonga.com>
2017-12-08 16:41:35 +0000

    Fetch test.

--- index.js
+++ index.js
@@ -2,11 +2,5 @@
 const fs = require('fs');
 
 exports.get = function(event, context, callback) {
-  const contents = fs.readFileSync("public/index.html");
-  const response = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: {'Content-Type': 'text/html'}
-  };
-  callback(null, response);
+  callback(null, fetch('https://reqres.in/api/users?page=2'));
 };
